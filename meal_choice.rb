c05a7caf591abfc02496ca5dc92fb03e076142fa
Adella <adellakenn@gmail.com>
2019-05-09 19:11:53 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,7 +2,14 @@
 # passed into it and defaults to meat.
 
 def meal_choice(protein = "meat")
+<<<<<<< HEAD
   protein
 end
   
 meal_choice()
+=======
+  puts #{protein}
+  meal_choice("vegan")
+end
+  
+>>>>>>> 3baaaab4f90992ce99ad33b8d48dac30fadd2301
